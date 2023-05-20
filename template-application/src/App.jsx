270e1e6bbbb845ee734c4db8d5ec5e525682e816
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-05-19 23:17:17 -0400

    Add routes to App,jsx file

--- template-application/src/App.jsx
+++ template-application/src/App.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
-import {Home, About, Projects, Contact, Error} from "./components"
+import { Home, About, Projects, Contact, Error } from "./components"
+
 import "./App.css"
 
 
 const App = () => {
   return (
     <BrowserRouter>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Projects />} />
+        <Route path="/" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </BrowserRouter>
   )
 }
