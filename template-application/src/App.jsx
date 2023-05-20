ba7f1faf71cf50ad4978fe6e510ccfc26560da42
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-05-19 23:46:42 -0400

    add nav bar & footer setup

--- template-application/src/App.jsx
+++ template-application/src/App.jsx
@@ -1,20 +1,67 @@
 import React, {useState} from 'react'
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
-import { Home, About, Projects, Contact, Error } from "./components"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import Error from "./components/Error"
 
 import "./App.css"
 
 
 const App = () => {
+  const [activeNavItem, setActiveNavItem] = useState("");
+
+  const handleNavItemClick = (navItem) => {
+    setActiveNavItem(navItem);
+  };
+
   return (
     <BrowserRouter>
+      <nav>
+        
+        <Link
+          to="/"
+          onClick={() => handleNavItemClick("Home")}
+          className={`nav-item ${activeNavItem === "Home" ? "active" : ""}`}
+        >
+          Home
+        </Link>
+
+        <Link
+          to="/about"
+          onClick={() => handleNavItemClick("About")}
+          className={`nav-item ${activeNavItem === "About" ? "active" : ""}`}
+        >
+          Our Team
+        </Link>
+
+        <Link
+          to="/projects"
+          onClick={() => handleNavItemClick("Projects")}
+          className={`nav-item ${activeNavItem === "Projects" ? "active" : ""}`}
+        >
+          Projects
+        </Link>
+
+        <Link
+          to="/contact"
+          onClick={() => handleNavItemClick("Contact")}
+          className={`nav-item ${activeNavItem === "Contact" ? "active" : ""}`}
+        >
+          Contact Us
+        </Link>
+
+      </nav>
+      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<About />} />
-        <Route path="/" element={<Projects />} />
-        <Route path="/" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <footer className="sticky-footer"> This is the footer</footer>
     </BrowserRouter>
   )
 }
